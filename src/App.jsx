9ae38c1e194527d4b754255d05fa9f4216f57491
Amelia Scarbrough <scarbroughm@hotmail.com>
2021-06-16 16:39:53 -0500

    create app and store

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+// import { bindActionCreators } from 'redux'
+import { decrease, increase } from './action'
+
+const App = () => {
+
+    const count = useSelector(state => state)
+    const dispatch = useDispatch()
+    // const {increase, decrease} = bindActionCreators(actionCreators, dispatch)
+
+
+    return (
+    <div>
+        <h1>{count}</h1>
+        <button onClick={() => dispatch(decrease())}>Decrease</button>
+        <button onClick={() => dispatch(increase())}>Increase</button>
+    </div>
+    )
+}
+
+export default App
